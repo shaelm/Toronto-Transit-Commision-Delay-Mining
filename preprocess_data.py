13e31e11e18eb3a7d6e8fda5baa0495cb89c1c05
shaelm <shael.minuk@gmail.com>
2019-11-11 15:13:17 -0600

    Combining Weather Data with StreetCar Data

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,4 +1,11 @@
 #to preprocess our loaded picled streetcar data for now
-import pandas
+import pandas as pd
 import pickle
+import datetime
 finaldata= pd.read_pickle("./finaldata.pkl")
+weatherdf=pd.read_csv("weatherstats_toronto_daily.csv")
+weatherdf['date']=pd.to_datetime(weatherdf['date'])
+weatherdf=weatherdf[['date','snow','avg_wind_speed']]
+weather_combined= pd.merge(finaldata,weatherdf,how='left',left_on='Report Date',right_on='date')
+weather_combined.to_excel('weatherCombined.xlsx', engine='xlsxwriter')
+
