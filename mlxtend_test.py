feba245e1d75e32275839e615a418a0751d09f03
Roy De Jesus <alddous1031@gmail.com>
2019-11-22 07:30:20 +0800

    Add association rules

--- mlxtend_test.py
+++ mlxtend_test.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import datetime
 from mlxtend.frequent_patterns import fpgrowth,apriori
+from mlxtend.frequent_patterns import association_rules
 
 weatherCombineddf= pd.read_pickle("processed_data/FinalCategorizedData.pkl")
 
@@ -15,5 +16,12 @@ weatherCombineddf=weatherCombineddf.drop(['date','TempCat'],axis=1)
 
 dummies = pd.get_dummies(weatherCombineddf)
 dummies = dummies.drop(["Min Delay", "snow", "avg_visibility", "avg_hourly_temperature","rain","month", "year"], axis=1)
+fp = fpgrowth(dummies, min_support=0.4, use_colnames=True)
 
-print(fpgrowth(dummies, min_support=0.4, use_colnames=True))
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('max_colwidth', -1)
+
+print(fp)
+
+print(association_rules(fp, metric="confidence", min_threshold=0.7))
